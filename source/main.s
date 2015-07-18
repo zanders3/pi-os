911dbce30e48eef7c018cf87c1e2af52836520cb
zanders3 <3zanders@gmail.com>
2015-07-18 20:52:16 +0100

    OK05 is now working

--- source/main.s
+++ source/main.s
@@ -11,19 +11,22 @@ mov r0,#16
 mov r1,#1
 bl SetGpioFunction
 
+ptrn .req r4
+ldr ptrn,=0b11111111101010100010001000101010
+seq .req r5
+mov seq,#0
+
 loop$:
 mov r0,#16
 mov r1,#1
+lsl r1,seq
+and r1,ptrn
 bl SetGpio
 
-ldr r0,=0xF4240
+ldr r0,=250000
 bl Wait
 
-mov r0,#16
-mov r1,#0
-bl SetGpio
-
-ldr r0,=0xF4240
-bl Wait
+add seq,#1
+and seq,#0b11111
 
 b loop$
